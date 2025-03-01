08eff2d01de7a1fbf5c8948ecb8fa14203535eeb
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2025-03-02 01:33:10 +0900

    update: おなかすいた

--- web/script.js
+++ web/script.js
@@ -13,3 +13,18 @@ function changeLocation(){
   
   // console.log("changeLocation()");
 }
+
+function update(){
+  document.getElementById("leave-1").  textContent = "8:10";
+  document.getElementById("arrive-1"). textContent = "9:10";
+  document.getElementById("waiting-1").textContent = "9:10";
+  document.getElementById("leave-1"). style.color = "#FF831F";
+  document.getElementById("arrive-1").style.color = "#FF831F";
+  document.getElementById("iwade-1").   style.flex = 1;
+  document.getElementById("wakayama-1").style.flex = 1;
+  document.getElementById("miyamae-1"). style.flex = 1;
+}
+
+leave-1
+arrive-1
+waiting-1
